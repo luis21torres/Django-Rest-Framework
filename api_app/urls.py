621b5d9a48c8b30a27858146088a68f468eeb4db
Luis Francisco <luisfranciscotorrespaiba@gmail.com>
2025-09-02 20:24:33 -0500

    Subir archivos al repositorio

---
+++ api_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    PersonaList, PersonaByDocumento,ActualizarPersona
+    TareaList, TareaByFecha, TareaByRangoFechas, TareaByPersona
+)
+
+urlpatterns = [
+    # Personas
+    path('personas/', PersonaList.as_view(), name='persona-list'),
+    path('personas/crear/', PersonaList.as_view(), name='persona-crear'),
+    path('personas/actualizar/<int:pk>/', ActualizarPersona.as_view(), name='persona-actualizar'),
+    path('personas/documento/<str:documento>/', PersonaByDocumento.as_view(), name='persona-por-documento'),
+]
+    
+   
---
+++ api_app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.shortcuts import get_object_or_404
+
+from .models import Persona
+from .serializers import PersonaSerializer
+
+class PersonaList(generics.ListCreateAPIView):
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+    def get(self, request):
+        personas = Persona.objects.all()
+        serializer = PersonaSerializer(personas, many=True)
+        if not personas:
+            raise NotFound('No se encontraron personas. ')
+        return Response({'success': True, 'detail': 'Listado de personas.', 'data': serializer.data}, status=status.HTTP_200_OK)
+# Crear personas
+class CrearPersona(generics.CreateAPIView):
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+    def post(self, request):
+        serializer = PersonaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'success': True, 'detail': 'Persona creada correctamente.', 'data': serializer.data},status=status.HTTP_201_CREATED)
+    
+#Actualizar personas
+class ActualizarPersona(generics.UpdateAPIView):
+    queryset = Persona.objects.all()
+    serializer_class = PersonaSerializer
+
+    def put(self, request, pk):
+        persona = get_object_or_404(Persona, pk=pk)
+        email = request.data.get('email', None)
+
+        # Verificar si el email ha cambiado
+        if email and email != persona.email:
+            # Verificar si ya existe otra persona con el mismo email
+            if Persona.objects.filter(email=email).exclude(pk=pk).exists():
+                return Response({'email': ['Persona with this email already exists. ']}, status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer = PersonaSerializer(persona, data=request.date)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({'success': True, 'detail': 'Persona actualizada correctamente. ', 'data': serializer.data}, status=status.HTTP_200_OK)
+        # Buscar persona por documento
+        class PersonaByDocumento(generics.ListAPIViwe):
+            serializer_class = PersonaSerializer
+
+            def get(self, request, documento):
+                persona = Persona.objects.filter(documento=documento).first()
+                if not persona:
+                    raise NotFound('No se encontro una persona con ese documento.')
+                serializer = PersonaSerializer(persona)
+                return Response({'success': True, 'datail': 'Persona encontrada.', 'data': serializer.data}, status=status.HTTP_200_OK)
+            
+
+
+
+      
